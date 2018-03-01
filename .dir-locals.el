42df4929efbdaab61298cf992cce5fafa10bed08
Lei Zhao <LeiZh26@gmail.com>
2018-03-01 14:27:33 +0800

    deploy gh-pages

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((web-mode
+  (web-mode-script-padding . 2)
+  (web-mode-style-padding . 2)
+  (web-mode-markup-indent-offset . 2)
+  (web-mode-css-indent-offset . 2)
+  (web-mode-code-indent-offset . 2))
+ (javascript-mode
+  (js-indent-level . 2))
+ (js-mode
+  (js-indent-level . 2))
+ ("bower_components"
+  (nil
+   (eval view-mode))
+  (dired-mode
+   (eval view-mode 0))))
